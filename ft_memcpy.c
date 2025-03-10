108fe6f55f488dafb9d43e68b7374a854b43a620
Mickaël Anderson <123479860+RaMiAnderson@users.noreply.github.com>
2025-03-10 20:58:22 +0300

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mramena <mramena@student.42antananarivo    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 15:12:31 by mramena           #+#    #+#             */
+/*   Updated: 2025/03/05 14:31:44 by mramena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char	*str_dest;
+	char	*str_src;
+	size_t	i;
+
+	str_dest = (char *)dest;
+	str_src = (char *)src;
+	i = 0;
+	while (i < n)
+	{
+		str_dest[i] = str_src[i];
+		i++;
+	}
+	return (dest);
+}
